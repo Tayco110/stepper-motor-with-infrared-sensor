580d470c7069a059e435da8a3ac20863e0c79df4
Rebeca Brandão <58791853+rebecabrandao1@users.noreply.github.com>
2022-08-31 07:47:58 -0300

    Update smirs-rebeca.ino

--- smirs-rebeca.ino
+++ smirs-rebeca.ino
@@ -1,175 +1,67 @@
-#include <AccelStepper.h>
-#include <IRremote.h>
+#include <Arduino.h>
+// #include <AccelStepper.h>
+// #include <IRremote.h>
 
-int IN1 = 8;   // seta os pinos da ponte h
-int IN2 = 9;
-int IN3 = 10;
-int IN4 = 11;
+int pinsPonteH[] = {8,9,10,11}; //pinos da ponte H
+int outputInfra = 6; //pino de retorno do infra
+int ledInfra = 13; //pino do led do infra
+
+int obstacle; //variável que recebe o valor lógico do IR
+int indexOut, indexIn; //variáveis auxiliares
+int atraso =  10; //variável para delay em ms usada na rotina
 int isObstacle = HIGH; // define obstáculo
 int isObstaclePin = 6; // define pino para obstáculo
 int LED = 13;
-void setup() 
-{
+
+char answer; //resposta da tag posicionada
+
+int debuguer = 1; //variável para debugar
+
+//valores lógicos para os passos no sentido horário
+int ponteHtableHorario[4][4] = {{1,0,0,1},{0,1,0,1},{0,1,1,0},{1,0,1,0}};
+
+//valores lógicos para os passos no sentido anti-horário
+int ponteHtableNHorario[4][4]= {{0,1,1,0},{0,1,0,1},{1,0,0,1},{1,0,1,0}};
+                            
+//setup
+void setup() {
+  Serial.begin(9600);
   pinMode(LED, OUTPUT);
   pinMode(isObstaclePin, INPUT);
-  Serial.begin(9600);
-  pinMode(IN1,OUTPUT); // declara cada pino como saída
-  pinMode(IN2,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
+  pinMode(obstacle, INPUT);
+  pinMode(ledInfra, OUTPUT);
+  for(indexOut = 0; indexOut < 4; indexOut ++){
+    pinMode(pinsPonteH[indexOut], OUTPUT);//seta os pinos de saída para a ponte H
+  }
 }
-int tempo = 10;// define tempo para movimentação do eixo
-void loop()
-{
-isObstacle = digitalRead(isObstaclePin);
-  //Passo 1
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 0);
-  //delay(tempo); 
-  //Passo 2
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 0);
-  //delay(tempo);
-  //Passo 3
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 0);
-  //delay(tempo);
-  //Passo 4
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 0);
-  //delay(tempo);
 
-  if(isObstacle == LOW){ // verifica se há um obstáculo (tag) se aproximando e interrompe o funcionamento do motor
+//código a ser rodado em loop
+void loop() {
+  isObstacle = digitalRead(isObstaclePin);
+
+  //Serial.println(debuguer);
+  //obstacle = digitalRead(outputInfra);
+  if(isObstacle == LOW){
+    for(indexOut = 0; indexOut < 4; indexOut ++){
+      for(indexIn = 0; indexIn < 4; indexIn ++){
+        digitalWrite(pinsPonteH[indexIn], ponteHtableNHorario[indexOut][indexIn]);
+       
+      } 
+     
+      delay(atraso);
+    }
+  }
+    if(isObstacle == HIGH){
+      for(indexOut = 0; indexOut < 4; indexOut ++){
+        for(indexIn = 0; indexIn < 4; indexIn ++){
+        digitalWrite(pinsPonteH[indexIn], ponteHtableHorario[indexOut][indexIn]);
       
-  //Serial.print("TAG DETECTADA");
-  digitalWrite(LED, HIGH);
-  //Passo 1
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo); 
-  //Passo 2
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo);
-  //Passo 3
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
-  //Passo 4
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
-  
-  
-        
-    
-}
-  
-    
-}
-
-
-
-**********************************************************************************
-
-Sentido anti-horário
-
-#include <AccelStepper.h>
-#include <IRremote.h>
-
-int IN1 = 8;   // seta os pinos da ponte h
-int IN2 = 9;
-int IN3 = 10;
-int IN4 = 11;
-int isObstacle = HIGH; // define obstáculo
-int isObstaclePin = 6; // define pino para obstáculo
-int LED = 13;
-void setup() 
-{
-  pinMode(LED, OUTPUT);
-  pinMode(isObstaclePin, INPUT);
-  Serial.begin(9600);
-  pinMode(IN1,OUTPUT); // declara cada pino como saída
-  pinMode(IN2,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
-}
-int tempo = 10;// define tempo para movimentação do eixo
-void loop()
-{
-isObstacle = digitalRead(isObstaclePin);
-  //Passo 1
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 0);
-  //delay(tempo); 
-  //Passo 2
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 0);
-  //delay(tempo);
-  //Passo 3
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 0);
-  //delay(tempo);
-  //Passo 4
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 0);
-  //delay(tempo);
-
-  if(isObstacle == LOW){ // verifica se há um obstáculo (tag) se aproximando e interrompe o funcionamento do motor
       
-  //Serial.print("TAG DETECTADA");
-  digitalWrite(LED, HIGH);
-
-// inverter 
-  //Passo 3
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
-  //Passo 2
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo);
-  
-  //Passo 1
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo); 
- 
-   //Passo 4
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
-  } 
+      
+      }
+       delay(atraso);
+  }
 }
+
+}
+
