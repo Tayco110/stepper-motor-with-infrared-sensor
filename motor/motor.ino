a523ce3865b2d04c5a0a63d213e5ee6547bf2f41
Roger <37402998+Roger987@users.noreply.github.com>
2022-10-06 10:13:10 -0300

    Add files via upload

--- motor/motor.ino
+++ motor/motor.ino
@@ -3,20 +3,26 @@
 #include <string.h>
  
 // Definição dos pinos 
-#define stepPin 2 //Passo eixo X
-#define dirPin 5 // Direção eixo X
+#define stepPinX 2 //Passo eixo X
+#define dirPinX 5 // Direção eixo X
+//#define stepPinY 3 //Passo eixo Y
+//#define dirPinY 6 // Direção eixo Y
 
 int PIR1 = 0;
 int flag_motor = 0;
 
 // tag pequena = 0, tag grande = 1
 int tagSize = 1;
+int delta = 0; // erro dos espaços entre as tags
+int flagFirstTag = 0;
 String incomingByte; // for incoming serial data
 
  void setup() {
  // Definindo ambos os pinos acima como saída
-   pinMode(stepPin,OUTPUT); 
-   pinMode(dirPin,OUTPUT);
+   pinMode(stepPinX,OUTPUT); 
+   pinMode(dirPinX,OUTPUT);
+/*   pinMode(stepPinY,OUTPUT); 
+   pinMode(dirPinY,OUTPUT);*/
    pinMode(A10, INPUT);
    Serial.begin(9600);
  }
@@ -26,13 +32,13 @@ String incomingByte; // for incoming serial data
   
   // Se apenas o sensor 1 for low vai ativar a flag de rotação do motor 1
   if (PIR1 < 50 && flag_motor == 0){
-    //Serial.println(flag_motor);
-    digitalWrite(dirPin,HIGH);
-    for(int x = 0; x < 630; x++) {
-      digitalWrite(stepPin,HIGH); 
-      delayMicroseconds(1500); 
-      digitalWrite(stepPin,LOW); 
-      delayMicroseconds(1500); 
+    Serial.println(flag_motor);
+    digitalWrite(dirPinX,HIGH);
+    for(int x = 0; x < 2570; x++) {
+      digitalWrite(stepPinX,HIGH); 
+      delayMicroseconds(300); 
+      digitalWrite(stepPinX,LOW); 
+      delayMicroseconds(300); 
      }
     flag_motor = 1;
   }
@@ -50,12 +56,12 @@ String incomingByte; // for incoming serial data
     // Se for uma tag com problema vai rebobinar
     if (incomingByte == "PROBLEMA\n"){
       Serial.println("Rebobinar motor");
-      digitalWrite(dirPin,LOW);
-      for(int x = 0; x < 650; x++) {
-        digitalWrite(stepPin,HIGH);
-        delayMicroseconds(500);
-        digitalWrite(stepPin,LOW);
-        delayMicroseconds(500);
+      digitalWrite(dirPinX,LOW);
+      for(int x = 0; x < 2600; x++) {
+        digitalWrite(stepPinX,HIGH);
+        delayMicroseconds(300);
+        digitalWrite(stepPinX,LOW);
+        delayMicroseconds(300);
       }
       //flag_motor = 0;
     }
@@ -63,24 +69,51 @@ String incomingByte; // for incoming serial data
     
     else if (incomingByte != "PROBLEMA\n" && tagSize == 0){
       Serial.println("Prosseguir");
-      digitalWrite(dirPin,HIGH);
-      for(int x = 0; x < 150; x++) {
-        digitalWrite(stepPin,HIGH); 
-        delayMicroseconds(1000); 
-        digitalWrite(stepPin,LOW); 
-        delayMicroseconds(500); 
+      if (flagFirstTag == 0){
+        delta = 775;
+        Serial.println("Primeira tag");
+        flagFirstTag = 1;
+      }
+      else{
+        delta = 775;
+      }
+      digitalWrite(dirPinX,HIGH);
+      Serial.println(delta);
+      for(int x = 0; x < delta; x++) {
+        digitalWrite(stepPinX,HIGH); 
+        delayMicroseconds(300); 
+        digitalWrite(stepPinX,LOW); 
+        delayMicroseconds(300); 
        }
+
+      /*digitalWrite(dirPinY,HIGH);
+      for(int x = 0; x < 1600; x++) {
+        digitalWrite(stepPinY,HIGH); 
+        delayMicroseconds(500); 
+        digitalWrite(stepPinY,LOW); 
+        delayMicroseconds(500); 
+       }*/
     }
 
+    // Tag grande
     else if (incomingByte != "PROBLEMA\n" && tagSize == 1){
       Serial.println("Prosseguir");
-      digitalWrite(dirPin,HIGH);
-      for(int x = 0; x < 150; x++) {
-        digitalWrite(stepPin,HIGH); 
-        delayMicroseconds(1000); 
-        digitalWrite(stepPin,LOW); 
-        delayMicroseconds(500); 
+      digitalWrite(dirPinX,HIGH);
+      Serial.println(delta);
+      for(int x = 0; x < 775; x++) {
+        digitalWrite(stepPinX,HIGH); 
+        delayMicroseconds(300); 
+        digitalWrite(stepPinX,LOW); 
+        delayMicroseconds(300); 
        }
+      /*digitalWrite(dirPinY,HIGH);
+      for(int x = 0; x < 1600; x++) {
+        digitalWrite(stepPinY,HIGH); 
+        delayMicroseconds(500); 
+        digitalWrite(stepPinY,LOW); 
+        delayMicroseconds(500); 
+       }*/
+      
     }
   }
   
